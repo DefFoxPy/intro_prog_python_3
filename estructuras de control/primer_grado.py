37709340a656071d9ba5ffbaf5ee6860ec7973b6
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2020-09-21 12:06:21 -0400

    corregir primer_grado.py
    evita la división entre cero con la sentencia if, linea 6

--- estructuras de control/primer_grado.py
+++ estructuras de control/primer_grado.py
@@ -3,6 +3,9 @@ print('Programa para la resolución de la ecuación a x + b = 0')
 a = float(input('Valor de a: '))
 b = float(input('Valor de b: '))
 
-x = -b / a
+if a != 0:
+	x = -b / a
+	print('Solución: ', x)
 
-print('Solución: ', x)
+if a == 0:
+	print('la ecuación no tiene solución')
