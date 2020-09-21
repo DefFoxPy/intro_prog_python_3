9428f8190d0de230b914ba506deb0a514fc3e4da
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2020-09-20 21:43:00 -0400

    Capitulo 3
    agregé los primeros programas del capitulo 3

---
+++ programas/ejercicio_32.py
@@ -0,0 +1,12 @@
+"""
+Diseña un programa que, a partir del valor del lado de un cuadrado (3 metros), muestre
+el valor de su perímetro (en metros) y el de su área (en metros cuadrados).
+(El perímetro debe darte 12 metros y el área 9 metros cuadrados)
+"""
+lado = 3
+
+perimetro = 4 * lado
+area = lado * lado
+
+print(perimetro)
+print(area)
---
+++ programas/ejercicio_34.py
@@ -0,0 +1,14 @@
+"""
+Diseña un programa que, a partir del valor de los dos lados de un rectángulo (4 y 6
+metros, respectivamente), muestre el valor de su perímetro (en metros) y el de su área (en metros
+cuadrados).
+(El perímetro debe darte 20 metros y el área 24 metros cuadrados)
+"""
+lado_1 = 4
+lado_2 = 6
+
+perimetro = lado_1 * 2 + lado_2 * 2
+area = lado_1 * lado_2
+
+print(perimetro)
+print(area)
