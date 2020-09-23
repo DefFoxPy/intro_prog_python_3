7968296621f8a341fc4ce9a969cfd8ffa4dc9549
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2020-09-23 10:45:43 -0400

    Update segundo_grado.py
    Evisa el posible error de división entre cero

--- estructuras de control/segundo_grado.py
+++ estructuras de control/segundo_grado.py
@@ -6,7 +6,10 @@ a = float(input('Valor de a: '))
 b = float(input('Valor de b: '))
 c = float(input('Valor de c: '))
 
-x1 = (-b + sqrt(b**2 - 4*a*c)) / (2 * a)
-x2 = (-b - sqrt(b**2 - 4*a*c)) / (2 * a)
+if a != 0:
+	x1 = (-b + sqrt(b**2 - 4*a*c)) / (2 * a)
+	x2 = (-b - sqrt(b**2 - 4*a*c)) / (2 * a)
+	print('Soluciones x1={0:.3f} y x2={1:.3f}'.format(x1, x2))
 
-print('Soluciones x1={0:.3f} y x2={1:.3f}'.format(x1, x2))
+if a == 0:
+	print('No es una ecuación de segundo grado.')
