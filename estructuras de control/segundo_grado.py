5cb038c380247fd2934a69adfe1a4fea46c0c000
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2020-09-27 18:07:02 -0400

    Update segundo_grado.py
    Evita el error de calcular la raíz de un número negativo

--- estructuras de control/segundo_grado.py
+++ estructuras de control/segundo_grado.py
@@ -7,10 +7,12 @@ b = float(input('Valor de b: '))
 c = float(input('Valor de c: '))
 
 if a != 0:
-	x1 = (-b + sqrt(b**2 - 4*a*c)) / (2 * a)
-	x2 = (-b - sqrt(b**2 - 4*a*c)) / (2 * a)
-	print('Soluciones x1={0:.3f} y x2={1:.3f}'.format(x1, x2))
-
+	if b ** 2 - 4 * a * c >= 0:
+		x1 = (-b + sqrt(b**2 - 4*a*c)) / (2 * a)
+		x2 = (-b - sqrt(b**2 - 4*a*c)) / (2 * a)
+		print('Soluciones x1={0:.3f} y x2={1:.3f}'.format(x1, x2))
+	else:
+		print('No hay soluciones reales.')
 else:
 	if b != 0:
 		x = -c / b
